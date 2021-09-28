ed5f84b3f464722cec2ac8e8ae0cd89c1dfd950d
yantaq <yantaq@att.net>
2021-09-28 13:06:59 -0700

    added nth fib func

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/yantaq/scratch/app"
 )
 
 func main() {
-	y := app.GCD(2, 4)
-	fmt.Printf("gcd(%d, %d) => %d", 2, 4, y)
+
+	var ops = flag.String("ops", "", "compute operations based on -ops flag value")
+	flag.Parse()
+
+	switch *ops {
+	case "gcd":
+		y := app.GCD(2, 4)
+		fmt.Printf("gcd(%d, %d) => %d", 2, 4, y)
+	case "nfib":
+		y := app.Fib(4)
+		fmt.Printf("nfib(%d) => %d", 4, y)
+	default:
+		fmt.Println("missing operations flag: --gcd, --nfib ...")
+	}
 }
